973b1421d23a91a1cab3d05891ee4a768dab659c
Matto <matt.mcquain@gmail.com>
2020-05-04 18:47:06 -0700

    working on the merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -17,8 +17,44 @@ def merge_sort(list):
   
   return merge(left, right)
   
+def split(list):
+  """
+  Divide the unsorted list at midpoint into sublists
+  Returns two sublists - left and right
+  """
   
+  mid = len(list)//2
+  left = list[:mid]
+  right = list[mid:]
   
+  return left, right
+  
+def merge(left, right):
+  """
+  Merges two lists, sorting them in the process
+  Returns a new merged list
+  """
+  
+  l = []
+  i = 0
+  j = 0
+  
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      l.append(left[i])
+      i+=1
+    else:
+      l.append(right[j])
+      j+=1
+      
+  while i < len(left):
+    l.append(left[i])
+    i+=1
+    
+  while j < len(right):
+    l.append(right[j])
+    j+=1
+
   
   
   
